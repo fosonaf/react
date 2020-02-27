037de961dc00367e15bf3385d5a49372277d928e
fzairidkt <fouad.zairi.partner@decathlon.com>
2020-02-27 14:32:38 +0100

    Prepare datatable exercice

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function Content() {
+
+  const [collabs, setCollabs] = useState([]);
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    const columns = [
+      { title: 'Name', field: 'name' },
+      { title: 'Surname', field: 'surname', initialEditValue: 'initial edit value' },
+      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+      {
+        title: 'Birth Place',
+        field: 'birthCity',
+        lookup: { 34: 'Fretin', 63: 'Marseille' },
+      },
+    ];
+
+    setColumns(columns);
+
+    const data = [
+      { name: 'Zidane', surname: 'Zinedine', birthYear: 1972, birthCity: 63 },
+      { name: 'Debuchy', surname: 'Mathieu', birthYear: 1985, birthCity: 34 },
+    ];
+
+    setCollabs(data);
+  });
+
   return (
     <div className="Content">
-      Content
+      TODO
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,14 @@ import './App.css';
 
 function Sidebar() {
 
+    const menuOne = () => {
+        alert("Menu one");
+    }
+
   return (
     <div className="Sidebar">
       <ul>
-          <h3 className="section">Menu 1</h3>
+          <h3 className="section" onClick={menuOne}>Menu 1</h3>
       </ul>
     </div>
   );
